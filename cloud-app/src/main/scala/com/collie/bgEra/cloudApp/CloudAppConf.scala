a999c4fdaa99bf06ac9e0827883e2950f0a349b6
sunwld <brlong521@163.com>
2018-06-27 12:39:49 +0800

    bgEra 1.0.0

--- cloud-app/src/main/scala/com/collie/bgEra/cloudApp/CloudAppConf.scala
+++ cloud-app/src/main/scala/com/collie/bgEra/cloudApp/CloudAppConf.scala
@@ -1,11 +1,18 @@
 package com.collie.bgEra.cloudApp
 
+import javax.sql.DataSource
 import org.mybatis.spring.annotation.MapperScan
+import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration, EnableAspectJAutoProxy}
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan(Array("com.collie.bgEra.cloudApp"))
 class CloudAppConf {
+
+//  @Autowired
+//  @Qualifier("mainDataSource")
+//  val mainDataSource: DataSource = null
+
 
 }
