58195c33b8619d23557af7307cfadc7ad13f105f
yi <yijindong2011@hotmail.com>
2018-06-27 09:32:30 +0800

    construct

--- cloud-app/src/main/scala/com/collie/bgEra/cloudApp/dtsf/bean/WorkUnitInfo.scala
+++ cloud-app/src/main/scala/com/collie/bgEra/cloudApp/dtsf/bean/WorkUnitInfo.scala
@@ -1,5 +1,12 @@
 package com.collie.bgEra.cloudApp.dtsf.bean
 
+/**
+  * 单个任务单元，是任务链中的一个部分。
+  * 任务单元信息中存放了任务的名称，相关任务目标的名称
+  * 运行的顺序ID
+  * 相关的资源MAP表
+  * 以及WORKUNIT相关的SPRING BEAN的名称，这些WORKUNIT BEAN必须实现了WORKUNITRUNABLE接口
+  */
 class WorkUnitInfo {
 
   var workUnitName: String = _
