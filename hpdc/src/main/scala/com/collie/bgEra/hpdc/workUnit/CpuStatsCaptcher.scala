2c895b63c960ab980796642d894ee4c3ab333c20
sunwld <brlong521@163.com>
2018-07-23 09:31:17 +0800

    Merge branch 'master' of https://github.com/sunwld/bgEra
    # Conflicts:
    #	cloud-app/src/main/scala/com/collie/bgEra/cloudApp/CloudAppConf.scala

--- hpdc/src/main/scala/com/collie/bgEra/hpdc/workUnit/CpuStatsCaptcher.scala
+++ hpdc/src/main/scala/com/collie/bgEra/hpdc/workUnit/CpuStatsCaptcher.scala
@@ -4,8 +4,7 @@ import java.util
 
 import com.collie.bgEra.cloudApp.dtsf.{ResourceManager, WorkUnitRunable}
 import com.collie.bgEra.cloudApp.dtsf.bean.WorkUnitInfo
-import com.collie.bgEra.cloudApp.ssh2Pool.{SshResult, SshSession}
-import org.apache.commons.pool2.impl.GenericObjectPool
+import com.collie.bgEra.cloudApp.ssh2Pool.{Ssh2Session, SshResult}
 import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
 import org.springframework.stereotype.Component
 
@@ -20,14 +19,13 @@ class CpuStatsCaptcher extends WorkUnitRunable{
 
   override def runWork(unit: WorkUnitInfo): Unit = {
     val cmd = shellMap.get("CPU.xsh")
-    val sshSessionPool: GenericObjectPool[SshSession] = resManager.getHostSshConnPoolResource(unit.getTargetId())
-    var session: SshSession = null
+    var session: Ssh2Session = null
     var sshResult: SshResult = null
     val rs:java.util.Map[String,Any] = new util.HashMap()
 
     val keys = Array("user", "sys", "wait", "idle")
     try {
-      session = sshSessionPool.borrowObject()
+      session = resManager.getHostSshConnPoolResource(unit.getTargetId())
       sshResult = session.execCommand(cmd)
       if(sshResult.getExitCode() == null || sshResult.getExitCode() == 0) keys.foreach(k => {
 
@@ -36,11 +34,7 @@ class CpuStatsCaptcher extends WorkUnitRunable{
       })
     } finally{
       if(session != null){
-        if(sshSessionPool == null){
-          session.closeSession()
-        }else{
-          sshSessionPool.returnObject(session)
-        }
+        session.close()
       }
     }
 
